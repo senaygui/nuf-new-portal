5e947996f8867ae025a12ccd68872b092ce1be98
senaygui <senaygeletaw@gmail.com>
2026-06-20 07:39:31 +0300

    update

---
+++ test_moodle_login.rb
@@ -0,0 +1,6 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+username = "cc/3977/26".downcase
+password = "some_password" # I don't know the password, I can't test actual login easily without a real user.
